6dd9bebdd094863753eca3236f8d54dc8bde91e9
dmegabyte <115552775+dmegabyte@users.noreply.github.com>
2024-07-08 20:29:57 +0300

    4

--- snake3.py
+++ snake3.py
@@ -5,25 +5,27 @@ import random
 clock = pygame.time.Clock()
 pygame.init()
 
-move = 'go down'
+move_list = ['go down','go right']
+
+move = ' '
 
 class Options:
     fps = 3
     WIDTH = 800
     HEIGHT = 700
     size = 50
+    score = 0
+
+class Game():
+    def __init__(self):
+        self.score = 0
+
 
 class Color:
     red = (255, 0, 0)
     green = (0, 255, 0)
 
-options = {
-    'FPS': 3,
-    'WIDTH': 800,
-    'HEIGHT': 800,
-    'GREEN': (0, 255, 0),
-    'SCORE': 0,
-}
+game = Game()
 
 keyscancode = {
     'UP': 82,
@@ -37,10 +39,13 @@ snake = [[0, 0],[0, 0]]
 
 def game_over():          
     for x,y in snake:
-        if (x >= Options.WIDTH or y >= Options.HEIGHT ) or (x < 0 or y < 0) or (len(snake)!= len(frozenset(map(tuple, snake)))):
-            screen_gameover(options)       
+        if (x >= Options.WIDTH or y >= Options.HEIGHT ) or (x < 0 or y < 0) :
+            screen_gameover(Options)
+        for i in range(2,len(snake)):
+            if snake[0] == snake[i]:
+                screen_gameover(Options)     
         else:
-            return options['FPS']  
+            return Options.fps 
 
 def move_function(head,move):
     if move == 'go up':
@@ -62,7 +67,7 @@ def random_num():
 
 def draw_snake(snake):       
     for x, y in snake:
-        pygame.draw.rect(screen, options['GREEN'], (x, y, Options.size, Options.size))
+        pygame.draw.rect(screen, Color.green, (x, y, Options.size, Options.size))
 def snake_move(snake,move):
     head = snake[0].copy()
     snake_copy = snake.copy()  
@@ -80,12 +85,16 @@ def screen_gameover(options):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+        text_size = 80
         screen.fill((0, 0, 0))
-        font = pygame.font.Font(None, 150)
+        font = pygame.font.Font(None, text_size)
         text_message = font.render(f'Game over', True, (255, 255, 255))
-        text_score = font.render(f'Score {options['SCORE']}', True, (255, 255, 255))
-        screen.blit(text_message, (210, 300))
-        screen.blit(text_score, (300, 500))
+        text_score = font.render(f'Score {Options.score}', True, (255, 255, 255))
+        text_rect = text_message.get_rect(center=(Options.WIDTH/2, Options.HEIGHT/2-100))
+        screen.blit(text_message, text_rect)
+
+        text_rect = text_score.get_rect(center=(Options.WIDTH/2, Options.HEIGHT/2 + text_size))
+        screen.blit(text_score, text_rect)
     
         pygame.display.flip()   
 def keydown(event,move):
@@ -106,39 +115,36 @@ def create_food():
     return food    
 food = create_food()        
 while True:
+    u = True
     for event in pygame.event.get():
+        
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        elif event.type == pygame.KEYDOWN:
+        elif event.type == pygame.KEYDOWN and u:
+            u = False
             move = keydown(event,move)
 
     
-    
-    
+
 #проверка столкновений
     if food == snake[0]:
         print(food,snake[0])
         snake.append(food)
         food = create_food()
-        options['FPS'] +=1
-        options['SCORE'] += 1
-                    
-            
-
+        Options.fps+=1
+        Options.score += 1
+                             
     font = pygame.font.Font(None, 50)
-    text = font.render(F"score {options['SCORE']}", True, (255, 255, 255))
+    text = font.render(F"score {Options.score}", True, (255, 255, 255))
     screen.blit(text, (0, 0))
     snake_move(snake,move)
-    options['FPS'] = game_over()
-
-
-
+    Options.fps = game_over()
 
     pygame.draw.rect(screen, Color.red, (food[0]+Options.size*0.2, food[1]+Options.size*0.2, Options.size*0.6, Options.size*0.6))
     draw_snake(snake)
 
     pygame.display.flip()                          
     
-    clock.tick(options['FPS'])
+    clock.tick(Options.fps)
     screen.fill((0, 0, 0))
